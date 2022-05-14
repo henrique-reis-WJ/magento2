9d2ab2ab3bd3bc9327bf27cfbab22793a02e7721
Henrique Naylor <henrique.lopes@webjump.com.br>
2022-05-14 15:58:15 -0300

    Modified modules.xml name, created first custom category attribute

--- app/code/Webjump/ConfigCategoryAttributes/Setup/Patch/Data/SetCategoryAttributes.php
+++ app/code/Webjump/ConfigCategoryAttributes/Setup/Patch/Data/SetCategoryAttributes.php
@@ -1,17 +1,24 @@
 <?php
+declare (strict_types = 1);
 namespace Webjump\ConfigCategoryAttributes\Setup\Patch\Data;
 
+use Magento\Eav\Model\Entity\Attribute\ScopedAttributeInterface;
+use Magento\Eav\Setup\EavSetup;
+use Magento\Eav\Setup\EavSetupFactory;
 use Magento\Framework\Setup\ModuleDataSetupInterface;
 use Magento\Framework\Setup\Patch\DataPatchInterface;
 
 class SetCategoryAttributes implements DataPatchInterface
 {
     private ModuleDataSetupInterface $moduleDataSetup;
+    private EavSetupFactory $eavSetupFactory;
 
     public function __construct(
-        ModuleDataSetupInterface $moduleDataSetup
+        ModuleDataSetupInterface $moduleDataSetup,
+        EavSetupFactory $eavSetupFactory
     ) {
         $this->moduleDataSetup = $moduleDataSetup;
+        $this->eavSetupFactory = $eavSetupFactory;
     }
 
     public function getAliases()
@@ -30,7 +37,17 @@ class SetCategoryAttributes implements DataPatchInterface
     {
         $this->moduleDataSetup->getConnection()->startSetup();
 
-        // Code
+        $eavSetup = $this->eavSetupFactory->create(['setup' => $this->moduleDataSetup]);
+        $eavSetup->addAttribute(\Magento\Catalog\Model\Category::ENTITY, 'product_temp_attribute', [
+            'type' => 'text',
+            'label' => 'Atributo de Categoria',
+            'input' => 'text',
+            'default' => 0,
+            'sort_order' => 5,
+            'global' => ScopedAttributeInterface::SCOPE_STORE,
+            'group' => 'General Information',
+            'visible_on_front' => true
+        ]);
 
         $this->moduleDataSetup->getConnection()->endSetup();
     }
