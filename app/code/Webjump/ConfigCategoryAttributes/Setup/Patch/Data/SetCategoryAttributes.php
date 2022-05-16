51cf73e96f2cd4d266227bdfb79bfbbdc8f4ab3b
Henrique Naylor <henrique.lopes@webjump.com.br>
2022-05-16 14:47:20 -0300

    Created Is Car and Is baloon atributes

--- app/code/Webjump/ConfigCategoryAttributes/Setup/Patch/Data/SetCategoryAttributes.php
+++ app/code/Webjump/ConfigCategoryAttributes/Setup/Patch/Data/SetCategoryAttributes.php
@@ -2,6 +2,7 @@
 declare (strict_types = 1);
 namespace Webjump\ConfigCategoryAttributes\Setup\Patch\Data;
 
+use Magento\Catalog\Model\Category;
 use Magento\Eav\Model\Entity\Attribute\ScopedAttributeInterface;
 use Magento\Eav\Setup\EavSetup;
 use Magento\Eav\Setup\EavSetupFactory;
@@ -10,6 +11,10 @@ use Magento\Framework\Setup\Patch\DataPatchInterface;
 
 class SetCategoryAttributes implements DataPatchInterface
 {
+    const ATTRIBUTE_AUTOMOTIVO = "Automotivo";
+    const ATTRIBUTE_MODA = "Festa";
+
+
     private ModuleDataSetupInterface $moduleDataSetup;
     private EavSetupFactory $eavSetupFactory;
 
@@ -38,11 +43,27 @@ class SetCategoryAttributes implements DataPatchInterface
         $this->moduleDataSetup->getConnection()->startSetup();
 
         $eavSetup = $this->eavSetupFactory->create(['setup' => $this->moduleDataSetup]);
-        $eavSetup->addAttribute(\Magento\Catalog\Model\Category::ENTITY, 'product_temp_attribute', [
-            'type' => 'text',
-            'label' => 'Atributo de Categoria',
-            'input' => 'text',
+        $eavSetup->addAttribute(Category::ENTITY,self::ATTRIBUTE_AUTOMOTIVO, [
+            'type' => 'int',
+            'label' => 'Is Car',
+            'input' => 'boolean',
+            'source'   => 'Magento\Eav\Model\Entity\Attribute\Source\Boolean',
             'default' => 0,
+            'required' => false,
+            'sort_order' => 5,
+            'global' => ScopedAttributeInterface::SCOPE_STORE,
+            'group' => 'General Information',
+            'visible_on_front' => true
+        ]);
+
+        $eavSetup = $this->eavSetupFactory->create(['setup' => $this->moduleDataSetup]);
+        $eavSetup->addAttribute(Category::ENTITY,self::ATTRIBUTE_AUTOMOTIVO, [
+            'type' => 'int',
+            'label' => 'Is baloon',
+            'input' => 'boolean',
+            'source'   => 'Magento\Eav\Model\Entity\Attribute\Source\Boolean',
+            'default' => 0,
+            'required' => false,
             'sort_order' => 5,
             'global' => ScopedAttributeInterface::SCOPE_STORE,
             'group' => 'General Information',
