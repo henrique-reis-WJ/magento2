089659d29d27fe2dbb11fae47a297539cf3d7633
Henrique Naylor <henrique.lopes@webjump.com.br>
2022-05-18 13:37:38 -0300

    writed SetNativePaymentGlobal base file

--- app/code/Webjump/ConfigNativePayment/Setup/Patch/Data/SetNativePaymentGlobal.php
+++ app/code/Webjump/ConfigNativePayment/Setup/Patch/Data/SetNativePaymentGlobal.php
@@ -5,25 +5,25 @@ namespace WebJump\ConfigNativePayment\Setup\Patch\Data;
 use Magento\Framework\App\Config\Storage\WriterInterface;
 use Magento\Framework\Setup\ModuleDataSetupInterface;
 use Magento\Framework\Setup\Patch\DataPatchInterface;
-use Magento\Framework\App\Config\ScopeConfigInterface;
+use Magento\Store\Model\StoreManagerInterface;
 
-class SetNativePayment implements DataPatchInterface 
+class SetNativePaymentGlobal implements DataPatchInterface 
 {
   //  public const SCOPE_INTERFACE
 
   private $moduleDataSetup;
   private $writer;
-  private $scopeConfig;
+  private $storeManager;
 
   public function __construct(
     ModuleDataSetupInterface $moduleDataSetup,
     WriterInterface $writer,
-    ScopeConfigInterface $scopeConfig
+    StoreManagerInterface $storeManager
   )
   {
     $this->moduleDataSetup = $moduleDataSetup;
     $this->writer = $writer;
-    $this->scopeConfig = $scopeConfig;
+    $this->storeManager = $storeManager;
   }
 
   public static function getDependencies()
@@ -34,7 +34,24 @@ class SetNativePayment implements DataPatchInterface
 
   public function apply() 
   {
+    $this->moduleDataSetup->getConnection()->startSetup();
 
+    $automotivoWebsiteId = $this->storeManager
+    ->getWebsite("automotivo")
+    ->getId();
+
+    $festasWebsiteId = $this->storeManager
+    ->getWebsite("festas")
+    ->getId();
+
+    $this->writer->save(
+      "payment/checkmo/active", 
+      "1", 
+      "websites",
+      $automotivoWebsiteId
+  );
+
+    $this->moduleDataSetup->getConnection()->endSetup();
   }
 
   public function getAliases()
