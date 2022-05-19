f5ea932b876f6680f663c35000eabcffec6ff4d6
Henrique Naylor <henrique.lopes@webjump.com.br>
2022-05-19 11:16:17 -0300

    created base files

---
+++ app/code/Webjump/ConfigCartPriceRules/Setup/Patch/Data/SetCartPriceRules.php
@@ -0,0 +1,37 @@
+<?php
+namespace Webjump\ConfigCartPriceRules\Setup\Patch\Data;
+
+use Magento\Framework\Setup\ModuleDataSetupInterface;
+use Magento\Framework\Setup\Patch\DataPatchInterface;
+
+class SetCartPriceRules implements DataPatchInterface
+{
+    private ModuleDataSetupInterface $moduleDataSetup;
+
+    public function __construct(
+        ModuleDataSetupInterface $moduleDataSetup
+        )
+    {
+        $this->moduleDataSetup = $moduleDataSetup;
+    }
+
+    public function getAliases()
+    {
+        return [
+        ];
+    }
+
+    public static function getDependencies()
+    {
+        return [
+        ];
+    }
+
+    public function apply() {
+        $this->moduleDataSetup->getConnection()->startSetup();
+
+
+        
+        $this->moduleDataSetup->getConnection()->endSetup();
+    }
+}
