7fafbb03b7a039049b93f885393e4a997142b65c
Henrique Naylor <henrique.lopes@webjump.com.br>
2022-05-19 16:14:47 -0300

    Added cart price rules

--- app/code/Webjump/ConfigCartPriceRules/Setup/Patch/Data/SetCartPriceRules.php
+++ app/code/Webjump/ConfigCartPriceRules/Setup/Patch/Data/SetCartPriceRules.php
@@ -35,7 +35,7 @@ class SetCartPriceRules implements DataPatchInterface
         ];
     }
 
-    public function createRule(string $name, string $description, string $discount, int $customerGroupId, int $websiteId){
+    public function createRuleCustomerGroup(string $name, string $description, string $discount, int $customerGroupId, int $websiteId){
         $ruleData = [
             "name" => $name,
             "description" => $description,
@@ -58,11 +58,63 @@ class SetCartPriceRules implements DataPatchInterface
             "use_auto_generation" => "0",
             "uses_per_coupon" => "0",
             "simple_free_shipping" => "0",
-            "customer_group_ids" => [$customerGroupId], // 0 = Not Logged, 1 = General, 2 = Wholesale, 4 = Retailer
+            "customer_group_ids" => [$customerGroupId], // 0 = Not Logged, 1 = General, 2 = Wholesale, 3 = Retailer
             "website_ids" => [$websiteId],
             "coupon_code" => null,
             "store_labels" => [],
             "conditions_serialized" => '',
+            "actions_serialized" => ''
+        ];
+
+         $ruleModel = $this->ruleFactory->create();
+         $ruleModel->setData($ruleData);
+         $ruleModel->save();
+    }
+
+    public function createRuleCart(string $name, string $description, string $discount){
+        $ruleData = [
+            "name" => $name,
+            "description" => $description,
+            "from_date" => null,
+            "to_date" => null,
+            "uses_per_customer" => "0",
+            "is_active" => "1",
+            "stop_rules_processing" => "1",
+            "is_advanced" => "1",
+            "product_ids" => null,
+            "sort_order" => "0",
+            "simple_action" => "cart_fixed",
+            "discount_amount" => $discount,
+            "discount_qty" => null,
+            "discount_step" => "0",
+            "apply_to_shipping" => "0",
+            "times_used" => "0",
+            "is_rss" => "1",
+            "coupon_type" => "1",
+            "use_auto_generation" => "0",
+            "uses_per_coupon" => "0",
+            "simple_free_shipping" => "0",
+            "customer_group_ids" => [0, 1, 2, 3], // 0 = Not Logged, 1 = General, 2 = Wholesale, 3 = Retailer
+            "website_ids" => [1, 2, 3],
+            "coupon_code" => null,
+            "store_labels" => [],
+            'conditions_serialized' => json_encode([
+                'type' => \Magento\SalesRule\Model\Rule\Condition\Combine::class,
+                'attribute' => null,
+                'operator' => null,
+                'value' => '1',
+                'is_value_processed' => null,
+                'aggregator' => 'all',
+                'conditions' => [
+                    [
+                        'type' => \Magento\SalesRule\Model\Rule\Condition\Address::class,
+                        'attribute' => 'total_qty',
+                        'operator' => '==',
+                        'value' => '5',
+                        'is_value_processed' => false,
+                    ],
+                ],
+            ]),
             "actions_serialized" => ''
         ];
 
@@ -76,7 +128,10 @@ class SetCartPriceRules implements DataPatchInterface
         
         $this->state->setAreaCode(\Magento\Framework\App\Area::AREA_FRONTEND);
 
-        $this->createRule("NotLoggedAutomotivo10", "Usuários não logados recebem 10% de desconto", "10.000", 0, 2);        $this->createRule("NotLoggedFestas5", "Usuários não logados recebem 5% de desconto", "5.000", 0, 3);
+        $this->createRuleCustomerGroup("NotLoggedAutomotivo10", "Usuários não logados recebem 10% de desconto", "10.000", 0, 2);        
+        $this->createRuleCustomerGroup("NotLoggedFestas5", "Usuários não logados recebem 5% de desconto", "5.000", 0, 3);
+        $this->createRuleCart("Cart5Itens10Discount", "Carrinho com 5 itens ou mais tem que ter 10% de desconto", "10.000");
+        
 
         $this->moduleDataSetup->getConnection()->endSetup();
     }
