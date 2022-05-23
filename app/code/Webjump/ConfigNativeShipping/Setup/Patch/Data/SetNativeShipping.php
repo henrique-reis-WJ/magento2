5e5c7bbbbbe5c4cac7b236d96e05c54b478b1e6e
Henrique Naylor <henrique.lopes@webjump.com.br>
2022-05-23 12:17:18 -0300

    Setted datapatch for default Shipping Setings

--- app/code/Webjump/ConfigNativeShipping/Setup/Patch/Data/SetNativeShipping.php
+++ app/code/Webjump/ConfigNativeShipping/Setup/Patch/Data/SetNativeShipping.php
@@ -3,16 +3,20 @@ namespace Webjump\ConfigNativeShipping\Setup\Patch\Data;
 
 use Magento\Framework\Setup\ModuleDataSetupInterface;
 use Magento\Framework\Setup\Patch\DataPatchInterface;
+use Magento\Framework\App\Config\Storage\WriterInterface;
 
 class SetNativeShipping implements DataPatchInterface
 {
     private ModuleDataSetupInterface $moduleDataSetup;
+    private WriterInterface $writer;
 
     public function __construct(
-        ModuleDataSetupInterface $moduleDataSetup
+        ModuleDataSetupInterface $moduleDataSetup,
+        WriterInterface $writer
         )
     {
         $this->moduleDataSetup = $moduleDataSetup;
+        $this->writer = $writer;
     }
 
     public function getAliases()
@@ -27,10 +31,84 @@ class SetNativeShipping implements DataPatchInterface
         ];
     }
 
+    public function setShippingSettings(string $websiteId) {
+       
+     $this->writer->save (
+        "carriers/tablerate/active",
+        "1",
+        "websites",
+        $websiteId
+    );
+
+    $this->writer->save (
+        "carriers/tablerate/title",
+        "Amazon",
+        "websites",
+        $websiteId
+    );
+
+    $this->writer->save (
+        "carriers/tablerate/name",
+        "Table Rate",
+        "websites",
+        $websiteId
+    );
+
+    $this->writer->save (
+        "carriers/tablerate/condition_name",
+        "package_weight",
+        "websites",
+        $websiteId
+    );
+    
+    $this->writer->save (
+        "carriers/tablerate/include_virtual_price",
+        "0",
+        "websites",
+        $websiteId
+    );
+
+    $this->writer->save (
+        "carriers/tablerate/handling_fee",
+        null,
+        "websites",
+        $websiteId
+    );
+
+    $this->writer->save (
+        "carriers/tablerate/sallowspecific",
+        "1",
+        "websites",
+        $websiteId
+    );
+
+    $this->writer->save (
+        "carriers/tablerate/specificcountry",
+        "BR,US",
+        "websites",
+        $websiteId
+    );
+
+    $this->writer->save (
+        "carriers/tablerate/showmethod",
+        "1",
+        "websites",
+        $websiteId
+    );
+
+    $this->writer->save (
+        "carriers/tablerate/sort_order",
+        "0",
+        "websites",
+        $websiteId
+    );
+
+    }
+
     public function apply() {
         $this->moduleDataSetup->getConnection()->startSetup();
         
-
+        $this->setShippingSettings(1);
 
         $this->moduleDataSetup->getConnection()->endSetup();
     }
