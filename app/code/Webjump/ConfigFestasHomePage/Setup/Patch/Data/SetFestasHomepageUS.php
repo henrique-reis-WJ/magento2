13732574c96006a68b6c625181a3f52ede9de35d
Henrique Naylor <henrique.lopes@webjump.com.br>
2022-05-25 12:52:16 -0300

    Created HomePage for US and BR Festas

--- app/code/Webjump/ConfigFestasHomePage/Setup/Patch/Data/SetFestasHomepageUS.php
+++ app/code/Webjump/ConfigFestasHomePage/Setup/Patch/Data/SetFestasHomepageUS.php
@@ -6,7 +6,7 @@ use Magento\Store\Model\StoreManagerInterface;
 use Magento\Framework\Setup\ModuleDataSetupInterface;
 use Magento\Framework\Setup\Patch\DataPatchInterface;
 
-class SetFestasHomepageBR implements DataPatchInterface
+class SetFestasHomepageUS implements DataPatchInterface
 {
     private ModuleDataSetupInterface $moduleDataSetup;
     private PageFactory $pageFactory;
@@ -40,15 +40,15 @@ public function apply()
     ->getId();
 
     $pageData = [
-        'title' => 'festasbr', // cms page title
+        'title' => 'festasus', // cms page title
         'page_layout' => 'cms-full-width', // cms page layout
         'meta_keywords' => '', // cms page meta keywords
         'meta_description' => '', // cms page meta description
-        'identifier' => 'festasbr', // cms page identifier
+        'identifier' => 'festasus', // cms page identifier
         'content_heading' => '', // cms page content heading
         'content' => '<div data-content-type="block" data-appearance="default" data-element="main">{{widget type="Magento\Cms\Block\Widget\Block" template="widget/static_block/default.phtml" block_id="5" type_name="CMS Static Block"}}</div><div data-content-type="block" data-appearance="default" data-element="main">{{widget type="Magento\Cms\Block\Widget\Block" template="widget/static_block/default.phtml" block_id="6" type_name="CMS Static Block"}}</div><div data-content-type="block" data-appearance="default" data-element="main">{{widget type="Magento\Cms\Block\Widget\Block" template="widget/static_block/default.phtml" block_id="8" type_name="CMS Static Block"}}</div>', // cms page content
         'layout_update_xml' => '', // cms page layout xml
-        'url_key' => 'festasbr', // cms page url key
+        'url_key' => 'party', // cms page url key
         'is_active' => 1, // status enabled or disabled
         'stores' => [$StoreViewGetId], // You can set store id single or multiple values in array.
         'sort_order' => 0, // cms page sort order
