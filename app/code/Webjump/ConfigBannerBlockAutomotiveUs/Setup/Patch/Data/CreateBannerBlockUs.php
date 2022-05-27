fbe5090bd33d1564377f32de4083d3f0f656206c
fabio <fabio.guedes@webjump.com.br>
2022-05-27 10:12:21 -0300

    Create block for US Store VIew Automotive

---
+++ app/code/Webjump/ConfigBannerBlockAutomotiveUs/Setup/Patch/Data/CreateBannerBlockUs.php
@@ -0,0 +1,89 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+namespace Webjump\ConfigBannerBlockAutomotiveUs\Setup\Patch\Data;
+use Magento\Cms\Model\BlockFactory;
+use Magento\Framework\Setup\ModuleDataSetupInterface;
+use Magento\Framework\Setup\Patch\DataPatchInterface;
+use Magento\Framework\Setup\Patch\PatchRevertableInterface;
+use Magento\Store\Model\StoreManagerInterface;
+class CreateBannerBlockUs implements DataPatchInterface, PatchRevertableInterface
+{
+    /**
+     * @var ModuleDataSetupInterface
+     */
+    private $moduleDataSetup;
+    /**
+     * @var BlockFactory
+     */
+    private $blockFactory;
+    const IDENTIFIER_BLOCK = 'banner-theme-automotive-us';
+    private StoreManagerInterface $storeManager;
+    /**
+     * CreateBannerBlock constructor.
+     * @param ModuleDataSetupInterface $moduleDataSetup
+     * @param BlockFactory $blockFactory
+     */
+    public function __construct(
+        ModuleDataSetupInterface $moduleDataSetup,
+        BlockFactory $blockFactory,
+        StoreManagerInterface $storeManager
+    ) {
+        $this->moduleDataSetup = $moduleDataSetup;
+        $this->blockFactory = $blockFactory;
+        $this->storeManager = $storeManager;
+    }
+    /**
+     * Patch Method "Apply" method $this->createBannerBlock()
+     * @return DataPatchInterface|void
+     */
+    public function apply()
+    {
+        $this->moduleDataSetup->getConnection()->startSetup();
+        $this->createBannerBlockUs("automotive_store_view_us");
+        $this->moduleDataSetup->getConnection()->endSetup();
+    }
+    /**
+     *  Method to create cms landing page
+     */
+    protected function createBannerBlockUs(string $storeCode)
+    {
+        $storeGetId = $this->storeManager->getStore($storeCode)->getId();
+        $cmsBlock = $this->blockFactory->create()->load(self::IDENTIFIER_BLOCK, 'identifier');
+        $cmsBlockData = [
+            'is_active' => 1,
+            'title' => 'Banner Theme Automotive US',
+            'identifier' => self::IDENTIFIER_BLOCK,
+            'stores' => [$storeGetId],
+            'content' => '<style>#html-body [data-pb-style=XVI85JM]{justify-content:flex-start;display:flex;flex-direction:column}#html-body [data-pb-style=VTOD581],#html-body [data-pb-style=XVI85JM]{background-position:center center;background-size:cover;background-repeat:no-repeat;background-attachment:scroll}#html-body [data-pb-style=T9PXU4I]{border-radius:0;min-height:560px;background-color:transparent}</style><div data-content-type="row" data-appearance="full-bleed" data-enable-parallax="0" data-parallax-speed="0.5" data-background-images="{}" data-background-type="image" data-video-loop="true" data-video-play-only-visible="true" data-video-lazy-load="true" data-video-fallback-src="" data-element="main" data-pb-style="XVI85JM"><div data-content-type="banner" data-appearance="poster" data-show-button="never" data-show-overlay="never" data-element="main" class="pagebuilder__banner-principal"><div data-element="empty_link"><div class="pagebuilder-banner-wrapper" data-background-images="{\&quot;desktop_image\&quot;:\&quot;{{media url=wysiwyg/image-banner-principal_1.png}}\&quot;,\&quot;mobile_image\&quot;:\&quot;{{media url=wysiwyg/image-banner-principal-mobile.png}}\&quot;}" data-background-type="image" data-video-loop="true" data-video-play-only-visible="true" data-video-lazy-load="true" data-video-fallback-src="" data-element="wrapper" data-pb-style="VTOD581"><div class="pagebuilder-overlay pagebuilder-poster-overlay" data-overlay-color="" aria-label="" title="" data-element="overlay" data-pb-style="T9PXU4I"><div class="pagebuilder-poster-content"><div data-element="content"></div></div></div></div></div></div></div>'
+        ];
+        if (!$cmsBlock->getId()) {
+            $this->blockFactory->create()->setData($cmsBlockData)->save();
+        } else {
+            $cmsBlock->setTitle($cmsBlockData['title']);
+            $cmsBlock->setContent($cmsBlockData['content']);
+            $cmsBlock->save();
+        }
+    }
+    /**
+     * @inheritdoc
+     */
+    public static function getDependencies()
+    {
+        return [];
+    }
+    public function revert()
+    {
+        $this->moduleDataSetup->getConnection()->startSetup();
+        $this->moduleDataSetup->getConnection()->endSetup();
+    }
+    /**
+     * @inheritdoc
+     */
+    public function getAliases()
+    {
+        return [];
+    }
+}
