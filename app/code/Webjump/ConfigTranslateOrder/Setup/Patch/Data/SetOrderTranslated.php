f7a45bc8a2d58c8696326d625b6e6fca645a1bd0
Henrique Naylor <henrique.lopes@webjump.com.br>
2022-06-02 13:50:26 -0300

    created base code

--- app/code/Webjump/ConfigTranslateOrder/Setup/Patch/Data/SetOrderTranslated.php
+++ app/code/Webjump/ConfigTranslateOrder/Setup/Patch/Data/SetOrderTranslated.php
@@ -3,16 +3,20 @@ namespace Webjump\ConfigTranslateOrder\Setup\Patch\Data;
 
 use Magento\Framework\Setup\ModuleDataSetupInterface;
 use Magento\Framework\Setup\Patch\DataPatchInterface;
+use Magento\Sales\Model\Order\StatusFactory;
 
 class SetOrderTranslated implements DataPatchInterface
 {
     private $moduleDataSetup;
+    private $statusFactory; 
 
     function __construct(
-        ModuleDataSetupInterface $moduleDataSetup
+        ModuleDataSetupInterface $moduleDataSetup,
+        StatusFactory $statusFactory
         )
     {
         $this->moduleDataSetup = $moduleDataSetup;
+        $this->statusFactory = $statusFactory;
     }
 
 
@@ -28,12 +32,26 @@ class SetOrderTranslated implements DataPatchInterface
         ];
     }
 
+    private function getStoreLabels(): array
+    {
+      return [
+         'store_view_id_1' => 'Label 1',
+         'store_view_id_2' => 'Label 2',
+      ];
+    }
+
     public function apply()
     {
         $this->moduleDataSetup->getConnection()->startSetup();
 
-
-
+        $code = 'status_code';
+        $status = $this->statusFactory->create()->load($code);
+        if(!$status->getStatus()) {
+           // lançar exceção
+        }
+        $status->setData('store_labels', $this->getStoreLabels());
+        $status->save();
+        
         $this->moduleDataSetup->getConnection()->endSetup();
     }
 }
